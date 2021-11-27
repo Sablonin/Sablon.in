65e5c317fbfd7a432040a83b4c96c1fcc98b5cfc
Farul Wananda <farulahmad2002@gmail.com>
2021-11-28 03:43:19 +0700

    menambah fitur akun

---
+++ websites/input-akun.php
@@ -0,0 +1,99 @@
+<?php include 'components/top-link.php'; ?>
+<?php include 'components/topbar.php'; ?>
+<?php include 'components/sidebar.php'; ?>
+
+<!-- Start Content Wrapper -->
+<div class="content-wrapper">
+    <div class="col-12 grid-margin">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Register</h4>
+                <?php include 'process/data-insert.php'; ?>
+                <form method="POST" class="form-sample">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group row">
+                                <label class="col-sm-3 col-form-label">Kode</label>
+                                <div class="col-sm-9">
+                                    <?php include 'process/generate-code.php' ?>
+                                    <input name="IDLogin" type="text" class="form-control" value="<?php echo GenerateLogin(); ?>" readonly />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group row">
+                                <label for="IDLevel" class="col-sm-3 col-form-label">Hak Akses</label>
+                                <div class="col-sm-9">
+                                    <select id="IDLevel" name="IDLevel" class="form-control" required>
+                                        <option value=""></option>
+                                        <?php
+                                        include 'process/data-option.php';
+                                        AmbilAkses(); ?>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group row">
+                                <label for="IDPegawai" class="col-sm-3 col-form-label">Nama Pegawai</label>
+                                <div class="col-sm-9">
+                                    <select name="IDPegawai" id="IDPegawai" class="form-control" required>
+                                        <option value=""></option>
+                                        <?php
+                                        AmbilNama(); ?>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group row">
+                                <label for="Password" class="col-sm-3 col-form-label">Password</label>
+                                <div class="col-sm-9">
+                                    <input id="Password" name="Password" type="password" class="form-control" minlength="2" maxlength="12" onChange="onChange()" placeholder="Password" required />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group row">
+                                <label for="Username" class="col-sm-3 col-form-label">Username</label>
+                                <div class="col-sm-9">
+                                    <input id="Username" name="Username" type="text" class="form-control" oninput="this.value = this.value.replace(/[^a-zA-z0-9]/, '')" minlength="6" maxlength="12" placeholder="Username" required />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group row">
+                                <label for="Confirm" class="col-sm-3 col-form-label">Confirm Password</label>
+                                <div class="col-sm-9">
+                                    <input id="Confirm" name="Confirm" type="password" class="form-control" minlength="2" maxlength="12" onChange="onChange()" placeholder="Confirm Password" required />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="btn btn-sm btn-primary" type="submit" name="tambah-akun">
+                        <i class="mdi mdi-account-plus"></i>Register
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Content Wrapper -->
+
+<?php include 'components/footer.php'; ?>
+<?php include 'components/bottom-link.php'; ?>
+<script>
+    function onChange() {
+        const password = document.querySelector('input[name=Password]');
+        const confirm = document.querySelector('input[name=Confirm]');
+        if (confirm.value === password.value) {
+            confirm.setCustomValidity('');
+        } else {
+            confirm.setCustomValidity('Passwords do not match');
+        }
+    }
+</script>
---
+++ websites/update-akun.php
@@ -0,0 +1,89 @@
+<?php include 'components/top-link.php'; ?>
+<?php include 'components/topbar.php'; ?>
+<?php include 'components/sidebar.php'; ?>
+
+<!-- Start Content Wrapper -->
+<div class="content-wrapper">
+    <div class="col-12 grid-margin">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Update Akun</h4>
+                <form method="POST" class="form-sample">
+                    <?php
+                    include '../configs/koneksi.php';
+                    $IDLogin = $_GET['IDLogin'];
+                    $GetAkun = mysqli_query($conn, "SELECT login.*, pegawai.Nama, akses.Akses FROM login INNER JOIN pegawai ON login.IDPegawai = pegawai.IDPegawai INNER JOIN akses ON login.IDLevel = akses.IDLevel WHERE IDLogin = '$IDLogin'");
+                    $DataAkun = mysqli_fetch_array($GetAkun); ?>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group row">
+                                <div class="col-sm-12">
+                                    <input name="IDLogin" type="text" class="form-control" value="<?php echo $DataAkun['IDLogin']; ?>" hidden />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group row">
+                                <label for="IDPegawai" class="col-sm-3 col-form-label">Nama Pegawai</label>
+                                <div class="col-sm-9">
+                                    <input name="IDPegawai" id="IDPegawai" class="form-control" value="<?php echo $DataAkun['Nama'] ?>" readonly>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group row">
+                                <label for="IDLevel" class="col-sm-3 col-form-label">Hak Akses</label>
+                                <div class="col-sm-9">
+                                    <input id="IDLevel" name="IDLevel" class="form-control" value="<?php echo $DataAkun['Akses']; ?>" readonly>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group row">
+                                <label for="Username" class="col-sm-3 col-form-label">Username</label>
+                                <div class="col-sm-9">
+                                    <input id="Username" name="Username" type="text" class="form-control" oninput="this.value = this.value.replace(/[^a-zA-z0-9]/, '')" minlength="6" maxlength="12" placeholder="Username" value="<?php echo $DataAkun['Username']; ?>" required />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group row">
+                                <label for="Password" class="col-sm-3 col-form-label">Password</label>
+                                <div class="col-sm-9">
+                                    <input id="Password" name="Password" type="text" class="form-control" minlength="2" maxlength="12" onChange="onChange()" placeholder="Password" value="<?php echo $DataAkun['Password']; ?>" required />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="btn btn-sm btn-primary" type="submit" name="update-akun">
+                        <i class="mdi mdi-account-plus"></i>Register
+                    </button>
+                    <a href="data-barang.php">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">
+                            <i class="mdi mdi-step-backward"></i>Kembali
+                        </button>
+                    </a>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Content Wrapper -->
+
+<?php include 'components/footer.php'; ?>
+<?php include 'components/bottom-link.php'; ?>
+<script>
+    function onChange() {
+        const password = document.querySelector('input[name=Password]');
+        const confirm = document.querySelector('input[name=Confirm]');
+        if (confirm.value === password.value) {
+            confirm.setCustomValidity('');
+        } else {
+            confirm.setCustomValidity('Passwords do not match');
+        }
+    }
+</script>
