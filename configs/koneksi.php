a156ff52306aa4445de2852dd8c1066a312e108f
farulwananda <farulahmad2002@gmail.com>
2021-12-30 20:33:12 +0700

    update konfigurasi koneksi

--- configs/koneksi.php
+++ configs/koneksi.php
@@ -1,2 +1,13 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "db_sablonin");
+//Database
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "db_sablonin";
+
+$conn = mysqli_connect($hostname, $username, $password, $database);
+
+//Cek koneksi
+if (!$conn) {
+  die("Database Tidak Terhubung : " . mysqli_connect_error());
+}
