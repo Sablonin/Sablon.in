82e072a166d1d826ba511fa1a23830984b0df165
farulwananda <farulahmad2002@gmail.com>
2022-01-05 09:39:39 +0700

    update view-ulasan

---
+++ configs/api/ulasan/view-ulasan.php
@@ -0,0 +1,29 @@
+<?php
+require '../../koneksi.php';
+header("Content-Type:application/json");
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+  $query_data = "SELECT * FROM ulasan";
+  $result = mysqli_query($conn, $query_data);
+  $json_array = array();
+  $response = "";
+
+  if (isset($result)) {
+    while ($row = mysqli_fetch_assoc($result)) {
+      $json_array[] = $row;
+    }
+    $response = array(
+      'code' => 200,
+      'status' => 'Successful',
+      'user_list' => $json_array
+    );
+  } else {
+    $response = array(
+      'code' => 400,
+      'status' => 'Error',
+      'user_list' => 0
+    );
+  }
+  print(json_encode($response));
+  mysqli_close($conn);
+}
